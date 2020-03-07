edcee8ea19dbaab36f3010a143f6add2b5008dc0
KryBielas <krystian-bielas@wp.pl>
2020-03-07 11:06:47 +0100

    firstfile

---
+++ WSB/PAI/1_struktura.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+
+<head>
+    <meta charaset="utf-8">
+    <tittle>Struktura dok</tittle>
+</head>
+<body>
+<?php
+ echo "test"
+ ?>
+</body>
+</html>
+
